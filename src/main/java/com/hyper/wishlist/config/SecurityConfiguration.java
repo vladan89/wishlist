790f4603c986a60ecd0bb41f3f1d30b4631b19a8
vladan.vulovic <vladan.vulovic@seavus.com>
2018-02-27 22:00:40 +0100

    removed all crap for login page, updated security request acceptance

--- src/main/java/com/hyper/wishlist/config/SecurityConfiguration.java
+++ src/main/java/com/hyper/wishlist/config/SecurityConfiguration.java
@@ -27,7 +27,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/**").permitAll()
+                .antMatchers("/built/**", "/css/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
